8baeaf0a2060ccb451a85353556ee376494d1d88
Oliver Woodings <oliver.woodings@gmail.com>
2016-01-21 13:52:36 +0000

    Make branch command use git rev-parse

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-'use strict';
-
-var fs = require('graceful-fs')
 var path = require('path')
 var childProcess = require('child_process')
 var shell = require('shelljs')
@@ -10,59 +7,33 @@ var PATH_SEP = path.sep
 var RE_BRANCH = /^ref: refs\/heads\/(.*)\n/
 
 function _command(cmd, args) {
-  var result;
+  var result
 
   if (HAS_NATIVE_EXECSYNC) {
-    result = childProcess.spawnSync(cmd, args);
+    result = childProcess.spawnSync(cmd, args)
 
     if (result.status !== 0) {
-      throw new Error('[git-rev-sync] failed to execute command: ' + result.error.code);
+      throw new Error('[git-rev-sync] failed to execute command: ' + result.error.code)
     }
 
-    return result.stdout.toString('utf8').replace(/^\s+|\s+$/g, '');
+    return result.stdout.toString('utf8').replace(/^\s+|\s+$/g, '')
   }
 
-  result = shell.exec(cmd + ' ' + args.join(' '), {silent: true});
+  result = shell.exec(cmd + ' ' + args.join(' '), {silent: true})
 
   if (result.code !== 0) {
-    throw new Error('[git-rev-sync] failed to execute command: ' + result.output);
+    throw new Error('[git-rev-sync] failed to execute command: ' + result.output)
   }
 
-  return result.output.toString('utf8').replace(/^\s+|\s+$/g, '');
-}
-
-function _getGitDirectory(start) {
-  start = start || module.parent.filename
-  if (typeof start === 'string') {
-    start = start.split(PATH_SEP)
-  }
-  start.pop()
-  var testPath = path.resolve(start.join(PATH_SEP), '.git')
-  if (fs.existsSync(testPath)) {
-    return testPath
-  }
-
-  if (!start.length) {
-    throw new Error('[git-rev-sync] no git repository found');
-  }
-
-  return _getGitDirectory(start)
+  return result.output.toString('utf8').replace(/^\s+|\s+$/g, '')
 }
 
 function branch () {
-  var gitDir = _getGitDirectory()
-  var head = fs.readFileSync(path.resolve(gitDir, 'HEAD'), 'utf8')
-  var b = head.match(RE_BRANCH)
-
-  if (b) {
-    return b[1]
-  }
-
-  return 'Detatched: ' + head.trim()
+  return _command('git', ['rev-parse', '--abbrev-ref', 'HEAD'])
 }
 
 function long() {
-  return _command('git', ['rev-parse', 'HEAD']);
+  return _command('git', ['rev-parse', 'HEAD'])
 }
 
 function short() {
@@ -74,7 +45,7 @@ function message() {
 }
 
 function tag() {
-  return _command('git', ['describe', '--always', '--tag', '--abbrev=0']);
+  return _command('git', ['describe', '--always', '--tag', '--abbrev=0'])
 }
 
 function log() {
